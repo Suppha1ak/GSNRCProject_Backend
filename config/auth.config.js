081fc54de8dca56146476f95f47306a69a299626
Suppha1ak <Supphalak.sm@gmail.com>
2023-10-26 13:57:42 +0700

    Suppha1ak

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    secret: "Shiro-Secret-Key",
+    secret: "GSRNProjects-Secret-Key",
     // jwtExpiration:3600,
     // jwtRefreshExpiration:86400,
     
     //test jwtRefreshExpiration
-    jwtExpiration:60, // 1minute
-    jwtRefreshExpiration:120, // 2minute
+    jwtExpiration:3600, // 1hours
+    jwtRefreshExpiration:3660, // 1hours 1minute
 }
--- middleware/auth.jwt.js
+++ middleware/auth.jwt.js
@@ -4,17 +4,17 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
 const db = require("../models");
 const User = db.user;
-const{TokenExpiretionError} = jwt
+const{TokenExpiredError} = jwt
 
 const catesError = (err , res) => {
-    if(err instanceof TokenExpiretionError){
+    if(err instanceof TokenExpiredError){
         return res.status(401).send({ message: "Unauthorized Access Token"});
     }
     return res.status(401).send({ message: "Unauthorized"});
 }
 
 verifyToken = (req, res , next) => {
-    let token = req.header['x-access-token'];
+    let token = req.headers['x-access-token'];
     if (!token) {
         return res.status(403).send({ message: "Invalid Token"});
     }
@@ -44,23 +44,6 @@ isAdmin = (req, res, next) => {
     })
 }
 
-isModerator = (req, res, next) => {
-    // select * from users where id = req.userId
-    User.findByPk(req.userId).then(user => {
-        // select * from Roles where userId = User_roles.userId and roleId = User_roles.roleId
-        user.getRoles().then((roles) => {
-            for(let i = 0; i < roles.length; i++) {
-                if(roles[i].name === "moderator"){
-                    next();
-                    return;
-                }
-            }
-            res.status(403).send({ message: "Require Moderator roles"});
-            return
-        })
-    })
-}
-
 isUser = (req, res, next) => {
     // select * from users where id = req.userId
     User.findByPk(req.userId).then(user => {
@@ -78,33 +61,10 @@ isUser = (req, res, next) => {
     })
 }
 
-isModeratorAdmin = (req, res, next) => {
-    // SELECT * FROM user WHERE id = req.body.userId
-    User.findByPk(req.userId).then((user) => {
-        // SELECT * FROM role, user, user_roles WHERE  user.id = users_roles.userId and role.id = users_roles.roleId
-        user.getRoles().then(roles => {
-            for (let i = 0; i < roles.length; i++) {
-                if (roles[i].name === "Moderator") {
-                    next();
-                    return;
-                }
-                if (roles[i].name === "admin") {
-                    next();
-                    return;
-                }
-            }
-            res.status(403).send({ message: "Require Moderator Role" });
-            return;
-        });
-    });
-};
-
 const authJWT = {
     verifyToken: verifyToken,
-    isModerator: isModerator,
     isUser: isUser,
     isAdmin: isAdmin,
-    isModeratorAdmin: this.isModeratorAdmin,
 }
 
 module.exports = authJWT;
