b2c6ba748f374db2d1be570715e9a5014412ebc0
Suttiporn Kaewsakunnee <644259025@webmail.npru.ac.th>
2023-10-30 22:18:35 +0700

    suttiporn_false

--- models/carcenter.model.js
+++ models/carcenter.model.js
@@ -49,7 +49,7 @@ const carcenter = sequelize.define("carcenter",{
 });
 
 // create database by sequelize and Delete table if trueeee
-carcenter.sync({force:true}).then(() => {
+carcenter.sync({force:false}).then(() => {
     console.log("Table is Create");
 }).catch((error) => {
     console.error("Error! Not create table");
