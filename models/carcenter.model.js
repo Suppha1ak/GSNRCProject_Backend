3a61284b34916a8c66d74e83b825cc730df26c91
Suttiporn Kaewsakunnee <644259025@webmail.npru.ac.th>
2023-10-30 21:48:27 +0700

    suttiporn_creat

--- models/carcenter.model.js
+++ models/carcenter.model.js
@@ -1,7 +1,7 @@
 const {dataType, DataTypes} = require("sequelize");
 const sequelize = require("./db");
 
-// creat id,brand
+// creat id,brand,model,price
 const carcenter = sequelize.define("carcenter",{
     id :{
         type: DataTypes.INTEGER,
