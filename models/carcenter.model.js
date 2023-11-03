ce6f98dc0866fb8f192514ef7b51e7232a22615b
AiinuShil3a <Supphalak.sm@outlook.com>
2023-11-03 18:19:53 +0700

    Suppha1ak

--- models/carcenter.model.js
+++ models/carcenter.model.js
@@ -56,11 +56,11 @@ const carcenter = sequelize.define("carcenter",{
     }
 });
 
-// create database by sequelize and Delete table if trueeee
-// carcenter.sync({force:true}).then(() => {
-//     console.log("Table is Create");
-// }).catch((error) => {
-//     console.error("Error! Not create table");
-// })
+//create database by sequelize and Delete table
+carcenter.sync({force:true}).then(() => {
+    console.log("Table is Create");
+}).catch((error) => {
+    console.error("Error! Not create table");
+})
 
 module.exports = carcenter;
